920bb34080c0fcb03bb8aa9eedc9b8daaff2f71a
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 16:27:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,6 +15,7 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete(array, string)
-  array.delete("string")
+def using_delete(array, steven)
+  array = "Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"
+  array.delete("steven")
 end
